3d6dd7b4c00709a2def807f4537407e7cf64ba9e
Hinna Parwez <78386492+HinnaP@users.noreply.github.com>
2024-10-02 18:15:09 -0400

    Update Wheel.h

--- Roullete/Wheel.h
+++ Roullete/Wheel.h
@@ -2,14 +2,14 @@
 #pragma once
 
 class Wheel {
-    public:
-        int bottomRange = 1; //Bottom range of values to spin from
-        int topRange = 10; //Top range of values to spin from
-        void setBottomRange(int n); //Setter for bottom range
-        void setTopRange(int n); //Setter for top range
-        void spin(); //Picks a random value in between the bottom and top range and assigns it to ball
-        void setBall(int n); //Setter for the value of the ball - we may not need this function 
-        int getBall(); //Getter for the value of the ball - we may not need this function
-    private:
-        int ball = 0; //Ball, intially defined to 0 but changes with spins
+public:
+    int bottomRange = 1; //Bottom range of values to spin from
+    int topRange = 10; //Top range of values to spin from
+    void setBottomRange(int n); //Setter for bottom range
+    void setTopRange(int n); //Setter for top range
+    void spin(); //Picks a random value in between the bottom and top range and assigns it to ball
+    void setBall(int n); //Setter for the value of the ball - we may not need this function 
+    int getBall(); //Getter for the value of the ball - we may not need this function
+private:
+    int ball = 0; //Ball, intially defined to 0 but changes with spins
 };
