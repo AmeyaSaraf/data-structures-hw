73c620ed96fd511da7030537109f6b1058658260
Hinna Parwez <78386492+HinnaP@users.noreply.github.com>
2024-10-02 18:14:11 -0400

    Update Player.cpp

--- Roullete/Player.cpp
+++ Roullete/Player.cpp
@@ -1,25 +1,25 @@
 #include "Player.h"
 
 // Constructor to initialize player's money
-P::P(int m) : $m(m), w() {}
+Player::Player(int m) : $m(m), w() {}
 
 // Setter for player's money
-void P::setM(int a) {
+void Player::setM(int a) {
     $m = a;
 }
 
 // Getter for player's money
-int P::getM() const {
+int Player::getM() const {
     return $m;
 }
 
 // Method to spin the wheel and get a value
-int P::sW() {
+int Player::sW() {
     w.spin();
     return w.getBall();
 }
 
 // Method to update player's money
-void P::uM(int a) {
+void Player::uM(int a) {
     $m += a;
 }
