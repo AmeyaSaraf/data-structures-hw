8afc18e4f06b82c6dd54ad26fac8cac34db051bb
Kaushiki143 <153568056+Kaushiki143@users.noreply.github.com>
2024-10-30 23:40:00 -0400

    Update Deck.cpp

--- War/Deck.cpp
+++ War/Deck.cpp
@@ -1,43 +1,42 @@
 #include "Deck.h"
 
-Deck::Deck() : head(nullptr), tail(nullptr), size(0) {}
+Deck::Deck() : h(nullptr), t(nullptr), s(0) {}
 
 Deck::~Deck() {
-    while (head != nullptr) {
-        Node* temp = head;
-        head = head->next;
+    while (h != nullptr) {
+        Node* temp = h;
+        h = h->n;
         delete temp;
     }
 }
 
-void Deck::addCard(int card) {
-    Node* newNode = new Node(card);
-    if (tail != nullptr) {
-        tail->next = newNode;
+void Deck::addCard(int c) {
+    Node* nN = new Node(c); // new node
+    if (t != nullptr) {
+        t->n = nN;
     }
-    tail = newNode;
-    if (head == nullptr) {
-        head = newNode;
+    t = nN;
+    if (h == nullptr) {
+        h = nN;
     }
-    size++;
+    s++;
 }
 
 int Deck::drawCard() {
-    if (head == nullptr) {
+    if (h == nullptr) {
         throw std::out_of_range("Deck is empty");
     }
-    Node* temp = head;
-    int card = head->card;
-    head = head->next;
-    if (head == nullptr) {
-        tail = nullptr;
+    Node* temp = h;
+    int c = h->c;
+    h = h->n;
+    if (h == nullptr) {
+        t = nullptr;
     }
     delete temp;
-    size--;
-    return card;
+    s--;
+    return c;
 }
 
 int Deck::countCards() const {
-    return size;
+    return s;
 }
-
