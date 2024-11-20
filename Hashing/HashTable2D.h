8fe1de05ca6911562b987d04ed6bfbd2576f4fcf
Hinna Parwez <78386492+HinnaP@users.noreply.github.com>
2024-11-20 12:58:38 -0500

    Create HashTable2D.h

---
+++ Hashing/HashTable2D.h
@@ -0,0 +1,21 @@
+#ifndef HASHTABLE2D_H
+#define HASHTABLE2D_H
+
+#include <iostream>
+
+class HashTable2D {
+private:
+    int table[100][5];
+    int counts[100]; // track how many items
+
+public:
+    HashTable2D();          // Constructor
+    ~HashTable2D();         // Destructor
+
+    int insert(int value);  // Insert function
+    int search(int value);  // Search function
+    int remove(int value);  // Remove function
+    void print();           // Print function
+};
+
+#endif
