e9feebeb87de5895b46ddf20e85b0402b3b90f96
Kaushiki Sonawane <153568056+Kaushiki143@users.noreply.github.com>
2024-11-20 12:33:59 -0500

    Update HashTable.cpp

--- Hashing/HashTable.cpp
+++ Hashing/HashTable.cpp
@@ -2,10 +2,14 @@
 #include <iostream>
 
 // Constructor to initialize hash table with given size
-HashTable::HashTable(int size) : size(size), table(size) {}
+HashTable::HashTable(int size) : size(size), table(size, nullptr) {}
 
-// Destructor
-HashTable::~HashTable() {}
+// Destructor to clean up resources
+HashTable::~HashTable() {
+    for (auto& ptr : table) {
+        delete ptr;
+    }
+}
 
 // Hash function to compute index based on value
 int HashTable::hash_function(int x) {
@@ -15,13 +19,13 @@ int HashTable::hash_function(int x) {
 // Repositions elements after removal to maintain correct order
 void HashTable::reposition(int removed_index) {
     int index = (removed_index + 1) % size;
-    while (table[index].has_value()) {
-        int original_index = hash_function(table[index].value());
-        if ((original_index <= removed_index && removed_index < index) || 
-            (removed_index < index && index < original_index) || 
+    while (table[index] != nullptr) {
+        int original_index = hash_function(*table[index]);
+        if ((original_index <= removed_index && removed_index < index) ||
+            (removed_index < index && index < original_index) ||
             (index < original_index && original_index <= removed_index)) {
             table[removed_index] = table[index];
-            table[index].reset();
+            table[index] = nullptr;
             removed_index = index;
         }
         index = (index + 1) % size;
@@ -32,18 +36,19 @@ void HashTable::reposition(int removed_index) {
 void HashTable::insert(int x) {
     int index = hash_function(x);
     int slots_checked = 0;
-    
+
     while (slots_checked < size) {
-        if (!table[index].has_value()) {
-            table[index] = x;
+        if (table[index] == nullptr) {
+            table[index] = new int(x);
             std::cout << "The value " << x << " was inserted." << std::endl;
             return;
-        } else {
+        }
+        else {
             index = (index + 1) % size;
             slots_checked++;
         }
     }
-    
+
     std::cout << "The value " << x << " cannot be inserted." << std::endl;
 }
 
@@ -53,13 +58,15 @@ void HashTable::search(int x) {
     int slots_checked = 0;
 
     while (slots_checked < size) {
-        if (!table[index].has_value()) {
+        if (table[index] == nullptr) {
             std::cout << "The value " << x << " cannot be found." << std::endl;
             return;
-        } else if (table[index] == x) {
+        }
+        else if (*table[index] == x) {
             std::cout << "The value " << x << " can be found." << std::endl;
             return;
-        } else {
+        }
+        else {
             index = (index + 1) % size;
             slots_checked++;
         }
@@ -74,15 +81,18 @@ void HashTable::remove(int x) {
     int slots_checked = 0;
 
     while (slots_checked < size) {
-        if (!table[index].has_value()) {
+        if (table[index] == nullptr) {
             std::cout << "The value " << x << " cannot be removed." << std::endl;
             return;
-        } else if (table[index] == x) {
-            table[index].reset();
+        }
+        else if (*table[index] == x) {
+            delete table[index];
+            table[index] = nullptr;
             reposition(index);
             std::cout << "The value " << x << " was removed." << std::endl;
             return;
-        } else {
+        }
+        else {
             index = (index + 1) % size;
             slots_checked++;
         }
@@ -94,10 +104,12 @@ void HashTable::remove(int x) {
 // Prints the contents of the hash table
 void HashTable::print_table() {
     for (int i = 0; i < size; ++i) {
-        if (table[i].has_value()) {
-            std::cout << "Slot " << i << ": " << table[i].value() << std::endl;
-        } else {
+        if (table[i] != nullptr) {
+            std::cout << "Slot " << i << ": " << *table[i] << std::endl;
+        }
+        else {
             std::cout << "Slot " << i << ": empty" << std::endl;
         }
     }
 }
+
