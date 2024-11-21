2c43a276b69ee76285fe33e086692766a171d2c5
Hinna Parwez <78386492+HinnaP@users.noreply.github.com>
2024-11-21 14:55:34 -0500

    Update HashTable2D.h

--- Hashing/HashTable2D.h
+++ Hashing/HashTable2D.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef HASHTABLE2D_H
 #define HASHTABLE2D_H
 
@@ -6,16 +7,16 @@
 class HashTable2D {
 private:
     int table[100][5];
-    int counts[100]; // track how many items
+    int counts[100]; // Track how many items are in each row
 
 public:
-    HashTable2D();          // Constructor
-    ~HashTable2D();         // Destructor
+    HashTable2D(); // Constructor
+    ~HashTable2D(); // Destructor
 
-    int insert(int value);  // Insert function
-    int search(int value);  // Search function
-    int remove(int value);  // Remove function
-    void print();           // Print function
+    int insert(int value); // Insert function
+    int search(int value); // Search function
+    int remove(int value); // Remove function
+    void print(); // Print function
 };
 
 #endif
