21369d1c888e8d39b212ffb149d6fa561d4d47ed
Hinna Parwez <78386492+HinnaP@users.noreply.github.com>
2024-11-21 14:56:19 -0500

    Update HashTable.cpp

--- Hashing/HashTable.cpp
+++ Hashing/HashTable.cpp
@@ -1,22 +1,18 @@
 #include "HashTable.h"
 #include <iostream>
 
-// Constructor to initialize hash table with given size
 HashTable::HashTable(int size) : size(size), table(size, nullptr) {}
 
-// Destructor to clean up resources
 HashTable::~HashTable() {
     for (auto& ptr : table) {
         delete ptr;
     }
 }
 
-// Hash function to compute index based on value
 int HashTable::hash_function(int x) {
     return x % size;
 }
 
-// Repositions elements after removal to maintain correct order
 void HashTable::reposition(int removed_index) {
     int index = (removed_index + 1) % size;
     while (table[index] != nullptr) {
@@ -32,76 +28,58 @@ void HashTable::reposition(int removed_index) {
     }
 }
 
-// Inserts a value into the hash table
-void HashTable::insert(int x) {
+int HashTable::insert(int x) {
     int index = hash_function(x);
     int slots_checked = 0;
 
     while (slots_checked < size) {
         if (table[index] == nullptr) {
             table[index] = new int(x);
-            std::cout << "The value " << x << " was inserted." << std::endl;
-            return;
+            return slots_checked + 1;
         }
-        else {
-            index = (index + 1) % size;
-            slots_checked++;
-        }
+        index = (index + 1) % size;
+        slots_checked++;
     }
-
-    std::cout << "The value " << x << " cannot be inserted." << std::endl;
+    return slots_checked;
 }
 
-// Searches for a value in the hash table
-void HashTable::search(int x) {
+int HashTable::search(int x) {
     int index = hash_function(x);
     int slots_checked = 0;
 
     while (slots_checked < size) {
         if (table[index] == nullptr) {
-            std::cout << "The value " << x << " cannot be found." << std::endl;
-            return;
+            return slots_checked;
         }
         else if (*table[index] == x) {
-            std::cout << "The value " << x << " can be found." << std::endl;
-            return;
+            return slots_checked + 1;
         }
-        else {
-            index = (index + 1) % size;
-            slots_checked++;
-        }
+        index = (index + 1) % size;
+        slots_checked++;
     }
-
-    std::cout << "The value " << x << " cannot be found." << std::endl;
+    return slots_checked;
 }
 
-// Removes a value from the hash table
-void HashTable::remove(int x) {
+int HashTable::remove(int x) {
     int index = hash_function(x);
     int slots_checked = 0;
 
     while (slots_checked < size) {
         if (table[index] == nullptr) {
-            std::cout << "The value " << x << " cannot be removed." << std::endl;
-            return;
+            return slots_checked;
         }
         else if (*table[index] == x) {
             delete table[index];
             table[index] = nullptr;
             reposition(index);
-            std::cout << "The value " << x << " was removed." << std::endl;
-            return;
+            return slots_checked + 1;
         }
-        else {
-            index = (index + 1) % size;
-            slots_checked++;
-        }
+        index = (index + 1) % size;
+        slots_checked++;
     }
-
-    std::cout << "The value " << x << " cannot be removed." << std::endl;
+    return slots_checked;
 }
 
-// Prints the contents of the hash table
 void HashTable::print_table() {
     for (int i = 0; i < size; ++i) {
         if (table[i] != nullptr) {
@@ -112,4 +90,3 @@ void HashTable::print_table() {
         }
     }
 }
-
